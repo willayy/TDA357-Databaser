b08f3a44659b8f1f02772ac8c3f327f04218dc80
William <117913560+willayy@users.noreply.github.com>
2024-01-26 16:38:11 +0100

    Update Labb1.sql

--- Labb1.sql
+++ Labb1.sql
@@ -176,9 +176,9 @@ CREATE VIEW CountedUncompleteMandatoryCourses AS (
 	WHERE Students.idnr = UncompleteMandatoryCourses.idnr
 );
 
-create View seminarCourses as(
-select studentpassedcourses.idnr, count(classified.course)
-from studentpassedcourses, classified
-where studentpassedcourses.course = classified.course and classified.classification = 'seminar'
-group by studentpassedcourses.idnr
+create View seminarCourses as (
+	select studentpassedcourses.idnr, count(classified.course)
+	from studentpassedcourses, classified
+	where studentpassedcourses.course = classified.course and classified.classification = 'seminar'
+	group by studentpassedcourses.idnr
 );
